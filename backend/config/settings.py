3198449efc80f5813ed9ff1dbc3024fccdde081b
Sunday <sunnexajayi@gmail.com>
2026-07-14 20:39:04 +0200

    feat: add neoinventory.vercel.app to CORS_ALLOWED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -192,6 +192,7 @@ else:
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://neoops.vercel.app",
+    "https://neoinventory.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
