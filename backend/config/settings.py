688f7a4da26b523ebe4672cb5eef6a79a309093b
Sunday <sunnexajayi@gmail.com>
2026-07-14 22:09:35 +0200

    feat: add production origin and regex pattern to CORS allowed settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -196,6 +196,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "https://ops.neoedgefusion.com"
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://\w+\.neoedgefusion\.com$",
 ]
 
 AUTH_USER_MODEL = 'users.User'
